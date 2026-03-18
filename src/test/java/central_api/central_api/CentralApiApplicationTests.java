fe897f05ceda1a5c317e6ca6d5ae50c2e606379d
Vicky Gupta <mrvickygupta1111@gmail.com>
2026-03-19 04:24:04 +0530

    pom.xml, gitIgnore and application.properties updated

---
+++ src/test/java/central_api/central_api/CentralApiApplicationTests.java
@@ -0,0 +1,13 @@
+package central_api.central_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
